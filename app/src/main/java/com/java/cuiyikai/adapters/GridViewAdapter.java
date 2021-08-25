0156cb056471f041545a5a74dfc3858df0d27de9
wwh <wuwh19@mails.tsinghua.edu.cn>
2021-08-25 11:06:13 +0800

    add the swipe

---
+++ app/src/main/java/com/java/cuiyikai/adapters/GridViewAdapter.java
@@ -0,0 +1,104 @@
+package com.java.cuiyikai.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.java.cuiyikai.R;
+
+import java.util.List;
+
+public class GridViewAdapter extends BaseAdapter {
+    private Context context;
+    private List<String> strList;
+    private int hidePosition = AdapterView.INVALID_POSITION;
+    private static boolean mIsEdit;
+    private int mType;
+
+    public GridViewAdapter(Context context, List<String> strList, int type) {
+        this.context = context;
+        this.strList = strList;
+        this.mType = type;
+    }
+
+    @Override
+    public int getCount() {
+        return strList.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return strList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+//    @Override
+//    public View getView(int i, View view, ViewGroup viewGroup) {
+//        return null;
+//    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        RelativeLayout layout = (RelativeLayout) LayoutInflater.from(context).inflate(
+                R.layout.channel_item, null);
+
+        TextView tv = layout.findViewById(R.id.text_item);
+        ImageView iv = layout.findViewById(R.id.iv_edit);
+        tv.setText(strList.get(position));
+        Log.v("grid", strList.get(position));
+        if(mIsEdit){
+            iv.setVisibility(View.VISIBLE);
+            if (mType == 0)
+                iv.setImageResource(R.drawable.x);
+            else
+                iv.setImageResource(R.drawable.add_channel);
+        }
+        else {
+            iv.setVisibility(View.INVISIBLE);
+        }
+
+        return layout;
+    }
+
+    public void hideView(int pos) {
+        hidePosition = pos;
+        notifyDataSetChanged();
+    }
+
+    public void showHideView() {
+        hidePosition = AdapterView.INVALID_POSITION;
+        notifyDataSetChanged();
+    }
+
+    public void removeView(int pos) {
+        strList.remove(pos);
+        notifyDataSetChanged();
+    }
+
+    //更新拖动时的gridView
+    public void swapView(int draggedPos, int destPos) {
+        //从前向后拖动，其他item依次前移
+        if(draggedPos < destPos) {
+            strList.add(destPos+1, getItem(draggedPos));
+            strList.remove(draggedPos);
+        }
+        //从后向前拖动，其他item依次后移
+        else if(draggedPos > destPos) {
+            strList.add(destPos, getItem(draggedPos));
+            strList.remove(draggedPos+1);
+        }
+        hidePosition = destPos;
+        notifyDataSetChanged();
+    }
+}
