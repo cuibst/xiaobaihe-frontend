f030afa43ac00490035dc8a5594af0f2f117759c
cuibst <cbst987@126.com>
2021-08-27 14:56:18 +0800

    Text to speech done?

--- app/src/main/java/com/java/cuiyikai/entities/PropertyEntity.java
+++ app/src/main/java/com/java/cuiyikai/entities/PropertyEntity.java
@@ -1,6 +1,8 @@
 package com.java.cuiyikai.entities;
 
-public class PropertyEntity {
+import androidx.annotation.Nullable;
+
+public class PropertyEntity implements Comparable<PropertyEntity> {
     private String label;
     private String object;
 
@@ -23,5 +25,24 @@ public class PropertyEntity {
     @Override
     public String toString() {
         return "Label: " + label + ", object: " + object;
+    }
+
+    @Override
+    public int hashCode() {
+        return label.hashCode() * object.hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return obj instanceof PropertyEntity && ((PropertyEntity)obj).label.equals(label) && ((PropertyEntity)obj).object.equals(object);
+    }
+
+    @Override
+    public int compareTo(PropertyEntity propertyEntity) {
+        if(!label.equals(propertyEntity.label))
+            return label.compareTo(propertyEntity.label);
+        if(!object.equals(propertyEntity.object))
+            return object.compareTo(propertyEntity.object);
+        return 0;
     }
 }
